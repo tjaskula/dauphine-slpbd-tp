b63c6c63b0624cf8c23bc2a8126f11790f86ef4b
Faouz El Fassi <faouz@drivy.com>
2017-11-10 09:47:20 +0100

    add solutions to TP2 sinks exercice

--- TP2/universal_sinks_and_sinks.scala
+++ TP2/universal_sinks_and_sinks.scala
@@ -1,8 +1,4 @@
-// Loading graph from file
-val g = sc.textFile("file:///Users/faouz/Documents/Cours/TP2/graph.txt")
-val g1 = g.map(x => ( x.split("\t")(0), x.split("\t")(2)))
-
-// Dario's solution
+/* A solution to universal sinks problem */
 // Example 1:
 val array = Array((1,4), (2,4), (3,4), (3,2))
 val g1 = sc.parallelize(array)
@@ -20,7 +16,7 @@ val n = nodes.distinct.count
 val sinks = indegree.filter(x => x._2 == n-1).map(_._1).collect
 
 
-// My solution
+/* A solution for max indegrees sink problem */
 val array = Array((1,2), (1,5), (1,6), (2,5), (2,6), (3,5), (3,6), (4,2), (4,5), (4,6))
 val g1 = sc.parallelize(array)
 val nodes = g1.flatMap(x => Array(x._1, x._2))
